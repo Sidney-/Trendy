f48349dec900f8d0baa2d8d639795a0fa6ead007
Sidney Eubanks <sidneyeubanks@icloud.com>
2014-08-05 16:31:52 -0700

    Mail form is now working
    its just not responsive yet

--- app/assets/javascripts/popUp.js
+++ app/assets/javascripts/popUp.js
@@ -1,29 +1,18 @@
 /**
  * Created by Armin on 8/5/14.
  */
-
 $(document).ready(function() {
-
     /* poopin up */
     $("#mail-list").on("click",function() {
         $.get('/newsletter', function(data){
-            $('.popi').html(data);
-            $('.popi').fadeIn();
-
-
+            $('#popi').html(data);
+            $('#popi').fadeIn();
         });
-
-
+        $('#popi').addClass ("pppp");
     });
-
 
     var slider = $('#slider').leanSlider({
         directionNav: '#slider-direction-nav',
         controlNav: '#slider-control-nav'
     });
-
-
-
-
-
 });
