4ecf1ea1ab483d0ea52e6c2214344a765d948bd9
Armin <armin.ak89@gmail.com>
2014-08-19 19:07:49 -0700

    Return Added.

--- app/assets/javascripts/sroller.js
+++ app/assets/javascripts/sroller.js
@@ -4,6 +4,11 @@
 $(document).ready(function(){
 
 
+    if ($( window ).width()>640)
+        $("#top-banner-text-large").text('MENTION THIS AD AND SAVE 20%! | (310) 392-0055');
+    if ($( window ).width()<640)
+        $("#top-banner-text-large").text('SHOP TRENDY (310) 392-0055');
+
 
     if(document.URL.search("\\?4")>0){
         console.log(document.URL) ;
@@ -40,4 +45,8 @@ $( window ).resize(function() {
 
     }
 
+    if ($( window ).width()>640)
+        $("#top-banner-text-large").text('MENTION THIS AD AND SAVE 20%! | (310) 392-0055');
+    if ($( window ).width()<640)
+        $("#top-banner-text-large").text('SHOP TRENDY (310) 392-0055');
 });
